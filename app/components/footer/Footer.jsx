d34a9570a598f5f4992c5273a56c6bfca4715d16
kushtriimhyseni <kushtrim@starlabspro.com>
2023-07-23 20:54:21 +0200

    Hot fixes

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import { RightContent } from "./RightContent";
 
 export const Footer = () => {
   return (
-    <div className="max-h-screen w-full flex-grow p-4 bg-[#10102b] bg-footer-bg bg-no-repeat grid grid-cols-1 lg:grid-cols-2 place-items-center">
+    <div className="max-h-screen w-full flex-grow p-4 bg-[#10102b] bg-footer-bg bg-no-repeat flex flex-col gap-8 lg:flex-row justify-center lg:justify-around items-center">
       <LeftContent />
       <RightContent />
     </div>

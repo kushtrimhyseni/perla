b0c3979cb3cf68e1b0bdae828b27e0b428e3f5c8
kushtriimhyseni <kushtrim@starlabspro.com>
2023-07-24 18:48:10 +0200

    Email updated

--- app/components/footer/RightContent.jsx
+++ app/components/footer/RightContent.jsx
@@ -4,7 +4,7 @@ export const RightContent = () => {
   return (
     <div className="flex flex-col items-center md:items-end">
       <span className="text-xl text-[#a9a9ca] font-bold">
-        perlaks@gmail.com
+        perlasmileks@gmail.com
       </span>
       <span className="text-[#a9a9ca] mt-4 flex justify-center md:justify-end">
         Powered by PERLA
